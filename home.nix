d1c049d2f84ca35845f63b0f7e8dccd05f118fd4
htgar <development@htgar.org>
2023-11-18 14:53:24 +0800

    Initial Commit

---
+++ home.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, ... }:
+
+{
+	home.username = "htgar";
+  home.homeDirectory = "/home/htgar";
+
+  home.stateVersion = "23.05";
+
+	news.display = "silent";
+
+  home.packages = [
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.gcc
+  ];
+
+  home.file = {
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Shell
+
+  programs.bash = {
+    enable = true;
+  };
+
+  programs.readline = {
+    enable = true;
+    extraConfig = builtins.readFile ./inputrc;
+  };
+
+	# Tools
+
+	# Starship
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+	# Bat
+	programs.bat = {
+		enable = true;
+		config = {
+			theme = "catppuccin-mocha";
+		};
+		themes = {
+			catppuccin-mocha = {
+				src = ./bat/themes;
+				file = "Catppuccin-mocha.tmTheme";
+			};
+		};
+  };
+
+  # Neovim
+  programs.neovim = {
+    enable = true;
+		defaultEditor = true;
+    extraLuaConfig = builtins.readFile ./nvim/init.lua;
+  };
+
+	# Git
+	programs.git = {
+		enable = true;
+		delta = {
+			enable = true;
+			options = {};
+		};
+		lfs.enable = true;
+		userName = "htgar";
+		userEmail = "development@htgar.org";
+	};
+	programs.git-credential-oauth.enable = true;
+  
+	# Tmux
+	programs.tmux = {
+		enable = true;
+	};
+}
+
+
